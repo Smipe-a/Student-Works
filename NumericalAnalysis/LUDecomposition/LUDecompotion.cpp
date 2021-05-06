d9b7e45984c27b634154ba4a5e0120d8d58b1f6b
Artem <smipe.artem@gmail.com>
2021-05-06 22:50:54 +0500

    add: to write simple-iteration method and find condition number
    Was writed Simple-Iteration method, implemented search condition number and fixed README.md for MethodGauss folder

--- NumericalAnalysis/LUDecomposition/LUDecompotion.cpp
+++ NumericalAnalysis/LUDecomposition/LUDecompotion.cpp
@@ -1,16 +1,87 @@
 /*
 * Laboratory work No.2
-*
+* Method LU-decomposition and simple-iteration method.
 * LU-decomposition. Simple-iteration method.
 * The solution is given for a 4x4 matrix.
 */
 #include <iostream>
 #include <iomanip>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 const int SIZE_MATRIX = 4;
+const double APPROX_EPSILON = 1e-6;
+
+// Overloading the multiplication operator for matrices (4x4)
+vector<vector<double>> operator*(const vector<vector<double>>& lhs_matrix, const vector<vector<double>>& rhs_matrix) {
+    vector<vector<double>> multiply_matrix(SIZE_MATRIX, vector<double>(SIZE_MATRIX));
+    for (int index_row = 0; index_row < SIZE_MATRIX; ++index_row) {
+        for (int index_col = 0; index_col < SIZE_MATRIX; ++index_col) {
+            for (int index_subrow = 0; index_subrow < SIZE_MATRIX; ++index_subrow) {
+                multiply_matrix[index_row][index_col] += lhs_matrix[index_row][index_subrow] * rhs_matrix[index_subrow][index_col];
+            }
+        }
+    }
+    return multiply_matrix;
+}
+
+// Overloading the multiplication operator for matrix (4x4) and vector (4x1)
+vector<double> operator*(const vector<vector<double>>& lhs_matrix, const vector<double>& rhs_vector) {
+    vector<double> multiply_matrix(SIZE_MATRIX);
+    for (int index_row = 0; index_row < SIZE_MATRIX; ++index_row) {
+        double value_multiply = 0.0;
+        for (int index_col = 0; index_col < SIZE_MATRIX; ++index_col) {
+            value_multiply += lhs_matrix[index_row][index_col] * rhs_vector[index_col];
+        }
+        multiply_matrix[index_row] = value_multiply;
+    }
+    return multiply_matrix;
+}
+
+// Overloading the multiplication operator for number and vector (4x1)
+vector<double> operator* (const double& number, const vector<double>& rhs_vector) {
+    vector<double> scale_vector(SIZE_MATRIX);
+    for (int index_element = 0; index_element < SIZE_MATRIX; ++index_element) {
+        scale_vector[index_element] = number * rhs_vector[index_element];
+    }
+    return scale_vector;
+}
+
+// Overloading the subtraction operator for vectors of dimension (4x1)
+vector<double> operator-(const vector<double>& lhs_vector, const vector<double>& rhs_vector) {
+    vector<double> subtraction_vector(SIZE_MATRIX);
+    for (int index_row = 0; index_row < SIZE_MATRIX; ++index_row) {
+        subtraction_vector[index_row] = lhs_vector[index_row] - rhs_vector[index_row];
+    }
+    return subtraction_vector;
+}
+
+void print_matrix(const vector<vector<double>>& output_matrix) {
+    cout << setprecision(2);
+    for (int index_row = 0; index_row < SIZE_MATRIX; ++index_row) {
+        for (int index_col = 0; index_col < SIZE_MATRIX; ++index_col) {
+            cout << setw(8) << left << fixed << output_matrix[index_row][index_col];
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+void print_vector(const vector<double>& output_vector, const string& name_vector) {
+    for (int index_element = 0; index_element < SIZE_MATRIX; ++index_element) {
+        if (name_vector == "Solution") {
+            cout << setprecision(7);
+            cout << "x_" << index_element + 1 << ": " << output_vector[index_element] << endl;
+        }
+        else if (name_vector == "Residual") {
+            cout << setprecision(15);
+            cout << "r_" << index_element + 1 << ": " << fixed << output_vector[index_element] << endl;
+        }
+    }
+    cout << endl;
+}
 
 class Equation {
 public:
@@ -70,31 +141,56 @@ void lu_decomposition(Equation& sles, vector<vector<double>>& matrix_L, vector<v
     sles.setSolutionVector(rhs_vector);
 }
 
-void print_matrix(const vector<vector<double>>& output_matrix) {
-    cout << setprecision(2);
-    for (int index_row = 0; index_row < SIZE_MATRIX; ++index_row) {
-        for (int index_col = 0; index_col < SIZE_MATRIX; ++index_col) {
-            cout << setw(8) << left << fixed << output_matrix[index_row][index_col];
-        }
-        cout << endl;
-    }
-    cout << endl;
+// Task No.2
+vector<double> find_residual_vector(Equation& sles) {
+    return sles.getMatrix() * sles.getSolutionVector() - sles.getRHSVector();
 }
 
-void print_vector(const vector<double>& output_vector, const string& name_vector) {
+// Task No.3
+double first_matrix_norm(const vector<vector<double>>& matrix) {
+    vector<double> quantity_sum_coloumns(SIZE_MATRIX);
+    for (int index_col = 0; index_col < SIZE_MATRIX; ++index_col) {
+        double sum_elements = 0.0;
+        for (int index_row = 0; index_row < SIZE_MATRIX; ++index_row) {
+            sum_elements += abs(matrix[index_row][index_col]);
+        }
+        quantity_sum_coloumns[index_col] = sum_elements;
+    }
+    return *max_element(quantity_sum_coloumns.begin(), quantity_sum_coloumns.end());
+}
+
+double second_vector_norm(const vector<double>& vector_by_norm) {
+    double sum_elements = 0.0;
     for (int index_element = 0; index_element < SIZE_MATRIX; ++index_element) {
-        if (name_vector == "Solution") {
-            cout << setprecision(4);
-            cout << "x_" << index_element + 1 << ": " << output_vector[index_element] << endl;
-        }
-        else if (name_vector == "Residual") {
-            cout << setprecision(15);
-            cout << "r_" << index_element + 1 << ": " << fixed << output_vector[index_element] << endl;
-        }
+        sum_elements += pow(vector_by_norm[index_element], 2);
     }
-    cout << endl;
+    return sqrt(sum_elements);
 }
 
+void simple_iteration_method(Equation& sles) {
+    const double TAU = 2.0 / first_matrix_norm(sles.getMatrix());
+    vector<double> prev_approximation(SIZE_MATRIX);
+    vector<double> rhs_vector = sles.getRHSVector();
+    int count_iteration = 0;
+    vector<double> next_approximation(SIZE_MATRIX);
+    for (int index_value = 0; index_value < SIZE_MATRIX; ++index_value) {
+        next_approximation[index_value] = static_cast<int>(rhs_vector[index_value]);
+    }
+    sles.setSolutionVector(next_approximation);
+    do {
+        prev_approximation = next_approximation;
+        next_approximation = prev_approximation - TAU * find_residual_vector(sles);
+        sles.setSolutionVector(next_approximation);
+        ++count_iteration;
+    } while (second_vector_norm(next_approximation - prev_approximation) > APPROX_EPSILON);
+    cout << "Quantity iteration: " << count_iteration << endl;
+    cout << endl << "Solution obtained by Simple-iteration method: " << endl;
+    print_vector(sles.getSolutionVector(), "Solution");
+    cout << "Residual vector obtained by Simple-iteration method: " << endl;
+    print_vector(find_residual_vector(sles), "Residual");
+}
+
+// Task No.4
 vector<vector<double>> find_inverse_matrix(vector<vector<double>> lhs_matrix) {
     //   Find the inverse matrix
     //   [ 15.0, 1.0, -5.0,   3.0  |  1.0, 0.0, 0.0, 0.0]
@@ -141,7 +237,6 @@ int main() {
                         -47.0,
                          28.0,
                         -50.0 });
-
     // Task No. 1
     vector<vector<double>> matrix_L(SIZE_MATRIX, { 0.0, 0.0, 0.0, 0.0 }), matrix_U(SIZE_MATRIX, { 0.0, 0.0, 0.0, 0.0 });
     lu_decomposition(sles, matrix_L, matrix_U);
@@ -152,7 +247,14 @@ int main() {
     cout << "Solution obtained by LU-decomposition: " << endl;
     print_vector(sles.getSolutionVector(), "Solution");
 
+    // Task No.2
+    cout << "Residual vector:" << endl;
+    print_vector(find_residual_vector(sles), "Residual");
+
+    // Task No.3
+    simple_iteration_method(sles);
+
     // Task No. 4
-    vector<vector<double>> inverse = find_inverse_matrix(sles.getMatrix());
+    cout << "Number condition matrix M_A: " << first_matrix_norm(sles.getMatrix()) * first_matrix_norm(find_inverse_matrix(sles.getMatrix()));
     return 0;
 }
--- NumericalAnalysis/MethodGauss/README.md
+++ NumericalAnalysis/MethodGauss/README.md
@@ -2,13 +2,13 @@
 ## Gaussian method with main element selection by row  
 
 <p align="left">
-    <img alt="SLEs" src="./sles_gauss.png" width="350"/>
+    <img alt="SLEs" src="img/sles_gauss.png" width="350"/>
 </p>
 
 ## Tasks: 
 
-1. It is required to solve the system <img alt="Equation" src="./sles_axf.png" width="51"/> of linear equations with the choice of the main element by rows.  
-2. Calculate the residual vector <img alt="Residual" src="./residual_gauss.png" width="80"/>, where <img alt="Solution" src="./solution_gauss.png" width="13"/> is the resulting solution.  
+1. It is required to solve the system <img alt="Equation" src="img/sles_axf.png" width="51"/> of linear equations with the choice of the main element by rows.  
+2. Calculate the residual vector <img alt="Residual" src="img/residual_gauss.png" width="80"/>, where <img alt="Solution" src="img/solution_gauss.png" width="13"/> is the resulting solution.  
 3. Calculate the determinant of a matrix using the Gaussian method.
-4. Find inverse matrix <img alt="Residual" src="./inverse_matrix.png" width="27"/> using Gaussian Method.
+4. Find inverse matrix <img alt="Residual" src="img/inverse_matrix.png" width="27"/> using Gaussian Method.
 5. Check inverse matrix, through multiply the source matrix by the resulting inverse matrix.
